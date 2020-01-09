1ddc74253627636ec660ef04ec7f9242fbfb6360
Kortunov Dmitry <kortunovdmitry@gmail.com>
2020-01-09 11:58:06 -0800

    Added task solution 'isPerfect'. Added tests for 'isPerfect' function.

--- intro_to_programming.js
+++ intro_to_programming.js
@@ -4,10 +4,11 @@
 каждый от 0 до 360,
 и возвращает наименьшую разницу между ними.
 
-diff(0, 45) === 45;         // не 315, а 45, потому что 45 меньше
-diff(0, 180) === 180;
-diff(0, 190) === 170;       // не 190, а 170, потому что 170 меньше
-diff(120, 280) === 160;
+Примеры:
+diff(0, 45) = 45;
+diff(0, 180) = 180;
+diff(0, 190) = 170;
+diff(120, 280) = 160;
 */
 exports.diff = (first, second) => {
   if (first < 0 || second < 0 || first > 360 || second > 360) {
@@ -23,4 +24,29 @@ exports.diff = (first, second) => {
   }
 
   return (diffFirst <= diffSecond) ? diffFirst : diffSecond;
+};
+
+/*
+Создайте функцию isPerfect,
+которая принимает число и возвращает true,
+если оно совершенное, и false — в ином случае.
+
+Совершенное число — положительное целое число,
+равное сумме его положительных делителей
+(не считая само число).
+Например, 6 — идеальное число, потому что 6 = 1 + 2 + 3.
+*/
+exports.isPerfect = (number) => {
+  if (number <= 5) {
+    return false;
+  }
+
+  let allDividerSum = 1;
+  for (let divider = 2; divider <= number / 2; divider += 1) {
+    if (number % divider === 0) {
+      allDividerSum += divider;
+    }
+  }
+
+  return (number === allDividerSum);
 };
--- tests/intro_to_programming.js
+++ tests/intro_to_programming.js
@@ -1,19 +1,52 @@
 const assert = require('assert');
-const { diff } = require('../intro_to_programming.js');
+const introToProgrammingModule = require('../intro_to_programming.js');
 
 describe('intro_to_programming:', () => {
   describe('test diff(first, second)', () => {
     it('should return 45 with (0, 45)', () => {
-      assert.equal(diff(0, 45), 45);
+      assert.equal(introToProgrammingModule.diff(0, 45), 45);
     });
     it('should return 180 with (0, 180)', () => {
-      assert.equal(diff(0, 180), 180);
+      assert.equal(introToProgrammingModule.diff(0, 180), 180);
     });
     it('should return 170 with (0, 190)', () => {
-      assert.equal(diff(0, 190), 170);
+      assert.equal(introToProgrammingModule.diff(0, 190), 170);
     });
     it('should return 160 with (120, 280)', () => {
-      assert.equal(diff(120, 280), 160);
+      assert.equal(introToProgrammingModule.diff(120, 280), 160);
+    });
+    it('should return 0 with (0, 0)', () => {
+      assert.equal(introToProgrammingModule.diff(0, 0), 0);
+    });
+    it('should return 0 with (360, 360)', () => {
+      assert.equal(introToProgrammingModule.diff(360, 360), 0);
+    });
+    it('should return 360 with (0, 360)', () => {
+      assert.equal(introToProgrammingModule.diff(0, 360), 0);
+    });
+  });
+
+  describe('test isPerfect(number)', () => {
+    it('should return false with (-1)', () => {
+      assert.ok(!introToProgrammingModule.isPerfect(-1));
+    });
+    it('should return false with (0)', () => {
+      assert.ok(!introToProgrammingModule.isPerfect(0));
+    });
+    it('should return false with (1)', () => {
+      assert.ok(!introToProgrammingModule.isPerfect(1));
+    });
+    it('should return false with (15)', () => {
+      assert.ok(!introToProgrammingModule.isPerfect(15));
+    });
+    it('should return true with (6)', () => {
+      assert.ok(introToProgrammingModule.isPerfect(6));
+    });
+    it('should return true with (28)', () => {
+      assert.ok(introToProgrammingModule.isPerfect(28));
+    });
+    it('should return true with (496)', () => {
+      assert.ok(introToProgrammingModule.isPerfect(496));
     });
   });
 });
